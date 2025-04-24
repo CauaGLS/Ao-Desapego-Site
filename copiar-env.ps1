48cae5f956612fa16ab48233781e1160f2b30c96
CauaGLS <cauagabriellima2004@gmail.com>
2025-04-24 14:36:49 -0300

    Removendo pasta do controle de versão

--- copiar-env.ps1
+++
@@ -1,9 +0,0 @@
-$backend = ".\backend\.env"
-$frontend = ".\frontend\.env"
-$origem = ".\.env"
-
-Copy-Item -Path $origem -Destination $backend -Force
-Copy-Item -Path $origem -Destination $frontend -Force
-
-
-Write-Host ".env atualizados"
